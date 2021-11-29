fbd121a32af2a241a63ffbd96abebe4462718c4e
mackliu <macklun@gmail.com>
2021-11-29 11:30:14 +0800

    增加一支計算符合條件筆數資料的函式rows(,)

--- frontend/vote.php
+++ frontend/vote.php
@@ -1,6 +1,19 @@
 <?php
 $id=$_GET['id'];
 $subject=find('topics',$id);
+
+$options=all('options',['topic_id'=>$id]);
+
 ?>
 
 <h1><?=$subject['topic'];?></h1>
+<ol>
+<?php
+foreach ($options as $key => $opt) {
+    echo "<li>";
+    echo $opt['opt'];
+
+    echo "</li>";
+}
+?>
+</ol>
