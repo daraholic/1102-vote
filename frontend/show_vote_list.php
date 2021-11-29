614833a526928a62c0a8246a2c54c1e9bd9dd8e6
mackliu <macklun@gmail.com>
2021-11-29 14:51:49 +0800

    美化首頁題目列表

--- frontend/show_vote_list.php
+++ frontend/show_vote_list.php
@@ -1,13 +1,26 @@
 <h1>列出所有的問題</h1>
 <?php
 $subjects=all('topics');
-echo "<ol>";
+echo "<ol class='list-group'>";
 foreach ($subjects as $key => $value) {
     if(rows('options',['topic_id'=>$value['id']]) > 0){
-    echo "<li>";
-    echo "<a href='index.php?do=vote&id={$value['id']}'>";
+    echo "<li class='list-group-item'>";
+    //題目
+    echo "<a class='d-inline-block col-md-8' href='index.php?do=vote&id={$value['id']}'>";
     echo $value['topic'];
     echo "</a>";
+    //總投票數顯示
+    
+    $count=q("select sum(`count`) as '總計' from `options` where `topic_id`='{$value['id']}'");
+    echo "<span class='d-inline-block col-md-2 text-center'>";
+    echo $count[0]['總計'];
+    echo "</span>";
+    
+    //看結果按鈕
+    echo "<span class='d-inline-block col-md-2 text-center'>";
+    echo "<button class='btn btn-primary'>觀看結果</button>";
+    echo "</span>";
+
     echo "</li>";
 }
 }
